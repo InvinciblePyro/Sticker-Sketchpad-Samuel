d15d4ae47af028f31b0ff5f3e3a3234a5b76dae5
InvinciblePyro <samrexspivey@gmail.com>
2025-10-15 10:00:13 -0700

    forgot to name the other uses of e to _e

--- src/main.ts
+++ src/main.ts
@@ -15,18 +15,18 @@ const cursor = { active: false, x: 0, y: 0 };
 
 canvas.addEventListener("mousedown", (_e) => {
   cursor.active = true;
-  cursor.x = e.offsetX;
-  cursor.y = e.offsetY;
+  cursor.x = _e.offsetX;
+  cursor.y = _e.offsetY;
 });
 
 canvas.addEventListener("mousemove", (_e) => {
   if (cursor.active) {
     ctx.beginPath();
     ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.lineTo(_e.offsetX, _e.offsetY);
     ctx.stroke();
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
+    cursor.x = _e.offsetX;
+    cursor.y = _e.offsetY;
   }
 });
 
